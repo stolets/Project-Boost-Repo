e8d6039d686b35b5fda03338643726f301575bf8
stolets <fraszczu@gmail.com>
2022-06-20 21:16:07 +0200

    Scene management
    next level
    restart from first level
    restart current level

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -11,17 +12,38 @@ public class CollisionHandler : MonoBehaviour
                 break;
 
             case "Finish":
+                NextLevel();
                 Debug.Log("Finish point reached");
                 break;
+
             case "Fuel":
                 Debug.Log("Re-fueled");
                 break;
+
             default:
+                ReloadLevel();
                 Debug.Log("Sorry, you blew up");
                 break;
 
 
         }
+    }
+
+    void ReloadLevel() 
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void NextLevel() 
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings) 
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 
 }
